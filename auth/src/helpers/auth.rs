906e01a8d783a41c6e742432aef86d19319f589c
tolumide-ng <tolumideshopein@gmail.com>
2022-03-04 02:00:57 +0100

    Update password to neccesitize number in the password

--- auth/src/helpers/auth.rs
+++ auth/src/helpers/auth.rs
@@ -37,7 +37,7 @@ impl Password {
     fn is_valid(pwd: &str) -> bool {
         // password must be atleast 8 characters with letters, numbers, and special char
         lazy_static! {
-             static ref RE: Regex = Regex::new(r#"^(?=.*[a-z])(?=.*[A-Z])(?=.*[~@#$%^&*+=`|{}:;!.?\(")\[\]-]).{8,}"#).unwrap();
+             static ref RE: Regex = Regex::new(r#"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~@#$%^&*+=`|{}:;!.?\(")\[\]-]).{8,}"#).unwrap();
         }
 
         RE.is_match(pwd).unwrap()
