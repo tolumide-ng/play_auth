4a24f5e1f68362e2aeae8393ebd78083b8456ee0
tolumide-ng <tolumideshopein@gmail.com>
2022-03-05 20:31:32 +0100

    Creates user and gives feedback on response

--- auth/src/base_repository/user.rs
+++ auth/src/base_repository/user.rs
@@ -15,32 +15,27 @@ struct Row {
 
 impl DbUser {
     pub async fn email_exist(pool: &Pool<Postgres>, email: String, username: String) -> bool {
-        // handle db connectioin errors with an interceptor
-        // let user = sqlx::query!(r#"SELECT * FROM play_user WHERE (email = $1)"#, email)
-        //     .fetch_optional(pool)
-        //     .await.unwrap();
-        let user = sqlx::query!(r#"SELECT email FROM play_user WHERE email = $1"#, email)
+        let user = sqlx::query!(r#"SELECT email FROM play_user WHERE (email = $1) OR (username = $2)"#, email, username)
             .fetch_optional(pool)
             .await.unwrap();
 
-        // let user = sqlx::query!(r#"SELECT email FROM play_user WHERE username = $1"#, username)
-        //     .fetch_optional(pool)
-        //     .await.unwrap();
+        if user.is_some() {
+            return false;
+        }
 
-        // println!("the users>>>>> {:#?}", user);
-
-        // if user.len() > 0 {
-        //     return true;
-        // }
-
-        return true;
+        return false;
     }
 
-    pub async fn create_user(pool: &Pool<Postgres>, email: String, username: String, hash: String) {
-        println!("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+    pub async fn create_user(pool: &Pool<Postgres>, email: String, username: String, hash: String) -> Option<bool> {
         let user = sqlx::query!(r#"INSERT INTO play_user (email, username, hash) VALUES ($1, $2, $3)"#, email, username, hash)
-            .execute(pool).await.unwrap();
+            .execute(pool).await;
 
-        println!("THE OBTAINED USER------------->>>>>>>>>>> {:#?}", user);
+        if let Err(e) = user {
+            // todo!() - this here can be handled with tracing! so we can save it on our log
+            println!("THE ERROR THAT OCCURED >>> {:#?}", e);
+            return None
+        }
+
+        return Some(true);
     }
 }
