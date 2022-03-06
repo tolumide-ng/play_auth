98166e37f5ba5735b2123a410861d8b32351ace8
tolumide-ng <tolumideshopein@gmail.com>
2022-03-06 16:28:37 +0100

    Updates base repository methods

--- auth/src/base_repository/user.rs
+++ auth/src/base_repository/user.rs
@@ -1,22 +1,36 @@
-use sqlx::{Pool, Postgres, error::DatabaseError};
+use sqlx::{Pool, Postgres};
 
 use crate::{helpers::commons::{Str, DbResult}, errors::app::ApiError};
 
 
 #[derive(Debug)]
-pub struct DbUser {
-    pub user_id: uuid::Uuid,
-    pub email: String,
-    pub hash: String,
+pub struct DbUser;
+
+pub struct User {
+    user_id: uuid::Uuid,
+    hash: String,
+    email: String,
+    verified: bool,
 }
 
-struct Row {
-    email: String,
-    username: String,
+impl User {
+    pub fn get_hash(&self) -> String {
+        self.hash
+    }
+
+    pub fn get_user(&self) -> (String, String) {
+        (self.email, self.user_id.to_string())
+    }
+
+    pub fn is_verified(&self) -> bool {
+        self.verified
+    }
+
 }
+
 
 impl DbUser {
-    pub async fn email_exist(pool: &Pool<Postgres>, email: String, username: String) -> DbResult<bool> {
+    pub async fn user_exist(pool: &Pool<Postgres>, email: String, username: String) -> DbResult<bool> {
         let user = sqlx::query!(r#"SELECT email FROM play_user WHERE (email = $1) OR (username = $2)"#, email, username)
             .fetch_optional(pool)
             .await;
@@ -30,9 +44,11 @@ impl DbUser {
         }
     }
 
-    pub async fn create_user(pool: &Pool<Postgres>, email: String, username: String, hash: String) -> DbResult<bool> {
-        let user = sqlx::query!(r#"INSERT INTO play_user (email, username, hash) VALUES ($1, $2, $3)"#, email, username, hash)
-            .execute(pool).await;
+    pub async fn create_user(pool: &Pool<Postgres>, email: String, hash: String) -> DbResult<bool> {
+        let user = sqlx::query!(r#"INSERT INTO play_user (email, hash) VALUES ($1, $2, $3) RETURNING user_id"#, email, hash)
+            .fetch_one(pool).await;
+
+        println!("THE INSERTED {:#?}", user);
 
         if let Err(e) = user {
             // todo!() - tracing!
@@ -40,5 +56,26 @@ impl DbUser {
         }
 
         return Ok(true);
+    }
+
+    pub async fn email_exists(pool: &Pool<Postgres>, email: String) -> DbResult<Option<User>> {
+        let user = sqlx::query_as!(User, r#"SELECT * FROM play_user WHERE (email = $1)"#)
+            .fetch_one(pool)
+            .await;
+
+        println!("DB RESPONSE {:#?}", user);
+        
+        match user {
+            Ok(the_user) => {
+                println!("THE RECEIVED USER {:#?}", the_user);
+                if the_user.is_some() {
+                    return Ok(Some(the_user));
+                }
+                return Ok(the_user)
+            },
+            Err(e) => {
+                return Err(ApiError::DatabaseError(e))
+            }
+        }
     }
 }
