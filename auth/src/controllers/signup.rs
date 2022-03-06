d7ece1107c362eb20bac4cabcb8ed64d19c847ec
tolumide-ng <tolumideshopein@gmail.com>
2022-03-06 16:36:41 +0100

    Updates signup endpoint not to require username on signup

--- auth/src/controllers/signup.rs
+++ auth/src/controllers/signup.rs
@@ -2,13 +2,13 @@ use rocket::{serde::json::Json, State};
 use serde::{Deserialize, Serialize};
 use sqlx::{Postgres, Pool};
 
-use crate::{base_repository::user::DbUser, settings::variables::EnvVars, helpers::{mail::{Email, MailType}, auth::Password, commons::{Str, ApiResult}}, response::{ApiSuccess}, errors::app::ApiError};
+use crate::{base_repository::user::DbUser, settings::variables::EnvVars, helpers::{mail::{Email, MailType}, auth::{Password, SignupJwt}, commons::{Str, ApiResult}}, response::{ApiSuccess}, errors::app::ApiError};
 
 
 #[derive(Deserialize, Serialize)]
 pub struct User {
     email: String,
-    username: String,
+    // username: String,
     password: String,
 }
 
@@ -19,19 +19,21 @@ pub struct User {
 pub async fn create(
     user: Json<User>, pool: &State<Pool<Postgres>>, _env: &State<EnvVars>
 ) -> ApiResult<Json<ApiSuccess<Str>>> {
-    let User {email, username, password} = user.0;
+    let User {email, password} = user.0;
 
-    let user_exists = DbUser::email_exist(pool, email.clone(), username.clone()).await?;
+    let user_exists = DbUser::email_exists(pool, email.clone()).await?;
 
-    if !user_exists {
+    if user_exists.is_none() {
         let pwd = Password::new(password.clone());
         match pwd {
             Some(hash) => {
                 // Password is ok and the username/email is unique
-                let user = DbUser::create_user(pool, email.clone(), username.clone(), hash.get_val()).await?;
+                let user = DbUser::create_user(pool, email.clone(), hash.get_val()).await?;
 
                 if user {
-                    Email::new(email, username, MailType::Signup("")).send_email();
+                    // generate jwt 
+                    // SignupJwt::new(signup_id)
+                    Email::new(email, None, MailType::Signup("")).send_email();
                     return Ok(ApiSuccess::reply_success(Some("Please check your email to verify your account")));
                 }
             },
