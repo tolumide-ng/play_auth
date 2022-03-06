02c157ea5dc824da015897d30ff780afb74c3664
tolumide-ng <tolumideshopein@gmail.com>
2022-03-06 16:29:30 +0100

    Adds login endpoint

---
+++ auth/src/controllers/login.rs
@@ -0,0 +1,38 @@
+use rocket::{serde::json::Json, State};
+use serde::{Deserialize, Serialize};
+use sqlx::{Postgres, Pool};
+
+use crate::{helpers::{commons::{ApiResult, Str}, auth::{Password, LoginJwt}}, response::ApiSuccess, base_repository::user::DbUser, errors::app::ApiError};
+
+
+#[derive(Deserialize, Serialize)]
+pub struct User {
+    email: String,
+    password: String,
+}
+
+#[post("/login", data = "<user>")]
+pub async fn user_login(
+    user: Json<User>,
+    pool: &State<Pool<Postgres>>,
+) -> ApiResult<Json<ApiSuccess<Str>>> {
+    let User { email, password } = user.0;
+
+    let user = DbUser::email_exists(pool, email).await?;
+
+    if let Some(db_user) = user {
+        if Password::is_same(db_user.get_hash(), password) {
+            if db_user.is_verified() {
+                let loginJwt = LoginJwt::new(email);
+                // return Ok(ApiSuccess::reply_success(body))
+                // let jwt = Login
+            }
+            return Err(ApiError::UnverifiedAccount)
+        }
+
+    }
+
+
+    // return Ok(ApiSuccess::reply_success(Some("Please check your email to verify your account")));
+    return Err(ApiError::AuthenticationError("Email or Password does not match"))
+}
