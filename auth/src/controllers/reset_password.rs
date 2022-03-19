1eead5a23298a0f958cb343cdf88cfd82ef367f6
tolumide-ng <tolumideshopein@gmail.com>
2022-03-19 13:07:45 +0100

    Allows only valid forgot password jwt_token to reset password for a user

--- auth/src/controllers/reset_password.rs
+++ auth/src/controllers/reset_password.rs
@@ -23,26 +23,35 @@ pub async fn reset(
 ) -> ApiResult<Json<ApiSuccess<&'static str>>> {
     let User {email, password, token} = user.0;
 
-    let token: TokenData<ForgotPasswordJwt> = ForgotPasswordJwt::decode(&token, &state.app)?;
+    println!("the provided token {:#?}", token);
+    let token_data: TokenData<ForgotPasswordJwt> = ForgotPasswordJwt::decode(&token, &state.app)?;
+
 
     let mut redis_conn = redis.get_async_connection().await?;
-    let user_id = token.claims.get_user();
+    let user_id = token_data.claims.get_user();
 
     let key = make_redis_key("forgot", user_id);
+
+    println!("THE CURRENT KEY {:#?}", key);
     let key_exists: Option<String> = redis_conn.get(&key).await?;
 
-    if key_exists.is_some() {
-        let parsed_email = Email::parse(email)?;
-        let parsed_password = Password::new(password, &state.app)?;
+    println!("THE KEY THAT CURRENTLY EXISTS {:#?}", key_exists);
 
-        DbUser::update_pwd(pool, parsed_password, parsed_email).await?;
-        // delete the forgot jwt token for this user
-        redis::cmd("DEL").arg(&[&key]).query_async(&mut redis_conn).await?;
-        // delete all current login_jwts for this user
-        let login_key = format!("{}:*", make_redis_key("login", user_id));
-        redis::cmd("DEL").arg(&[&login_key]).query_async(&mut redis_conn).await?;
 
-        return Ok(ApiSuccess::reply_success(Some("password reset successful")));
+    if let Some(data) = key_exists {
+        if data == token {
+            let parsed_email = Email::parse(email)?;
+            let parsed_password = Password::new(password, &state.app)?;
+    
+            DbUser::update_pwd(pool, parsed_password, parsed_email).await?;
+            // delete the forgot jwt token for this user
+            redis::cmd("DEL").arg(&[&key]).query_async(&mut redis_conn).await?;
+            // delete all current login_jwts for this user
+            let login_key = format!("{}:*", make_redis_key("login", user_id));
+            redis::cmd("DEL").arg(&[&login_key]).query_async(&mut redis_conn).await?;
+    
+            return Ok(ApiSuccess::reply_success(Some("password reset successful")));
+        }
     }
 
     Err(ApiError::AuthorizationError("Token is either expired or invalid"))
